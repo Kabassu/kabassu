800abd70863263d90d61422f99835e197f576af1
Daniel Madejek <dmadejek@gmail.com>
2018-01-25 22:03:13 +0100

    #1 Added base gradle file, Launcher

---
+++ kabassu-manager/src/main/java/io/kabassu/manager/KabassuManagerVerticle.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2016 Cognifide Limited
+ * Modified (C) 2018 Kabassu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.kabassu.manager;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableTransformer;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.core.AbstractVerticle;
+import org.apache.commons.lang3.tuple.Pair;
+
+
+public class KabassuManagerVerticle extends AbstractVerticle {
+
+  private static final String CONFIG_OVERRIDE = "config";
+  private static final String MODULE_OPTIONS = "options";
+  private static final Logger LOGGER = LoggerFactory.getLogger(KabassuManagerVerticle.class);
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+
+    Observable.fromIterable(config().getJsonArray("modules"))
+        .flatMap(module -> deployVerticle(module)
+            .onErrorResumeNext(
+                throwable -> (Observable<Pair<String, String>>) verticleCouldNotBeDeployed(module,
+                    throwable))
+        )
+        .compose(joinDeployments())
+        .subscribe(
+            message -> {
+              LOGGER.info("Kabassu STARTED {}", message);
+              startFuture.complete();
+            },
+            error -> {
+              LOGGER.error("Verticle could not be deployed", error);
+              startFuture.fail(error);
+            }
+        );
+  }
+
+  private Observable<Pair<String, String>> verticleCouldNotBeDeployed(Object module,
+      Throwable throwable) {
+    LOGGER.warn("Can't deploy {}: {}", module, throwable.getMessage());
+    return Observable.empty();
+  }
+
+  private Observable<Pair<String, String>> deployVerticle(final Object module) {
+    return vertx.rxDeployVerticle((String) module, getModuleOptions((String) module))
+        .map(deploymentID -> Pair.of((String) module, deploymentID))
+        .toObservable();
+  }
+
+  private DeploymentOptions getModuleOptions(final String module) {
+    DeploymentOptions deploymentOptions = new DeploymentOptions();
+    if (config().containsKey(CONFIG_OVERRIDE) && config().getJsonObject(CONFIG_OVERRIDE)
+        .containsKey(module)) {
+      //TODO: Other way for providing custom configuration
+      JsonObject moduleConfig = config().getJsonObject(CONFIG_OVERRIDE).getJsonObject(module);
+      if (moduleConfig.containsKey(MODULE_OPTIONS)) {
+        deploymentOptions.fromJson(moduleConfig.getJsonObject(MODULE_OPTIONS));
+      }
+    }
+    return deploymentOptions;
+  }
+
+  private ObservableTransformer<Pair<String, String>, String> joinDeployments() {
+    return observable ->
+        observable.reduce(new StringBuilder(System.lineSeparator()).append(System.lineSeparator()),
+            this::collectDeployment)
+            .toObservable()
+            .map(StringBuilder::toString);
+  }
+
+  private StringBuilder collectDeployment(StringBuilder accumulator,
+      Pair<String, String> deploymentId) {
+    return accumulator
+        .append(
+            String.format("\t\tDeployed %s [%s]", deploymentId.getRight(), deploymentId.getLeft()))
+        .append(System.lineSeparator());
+  }
+}
---
+++ kabassu-manager/src/main/java/io/kabassu/manager/factory/AbstractVerticleFactory.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2016 Cognifide Limited
+ * Modified (C) 2018 Kabassu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.kabassu.manager.factory;
+
+import io.kabassu.manager.utils.JsonObjectUtil;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Verticle;
+import io.vertx.core.json.DecodeException;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.spi.VerticleFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+public abstract class AbstractVerticleFactory implements VerticleFactory {
+
+
+  private static final String CONFIG_KEY = "config";
+  private static final String OPTIONS_KEY = "options";
+
+  @Override
+  public boolean requiresResolve() {
+    return true;
+  }
+
+  @Override
+  public void resolve(String id, DeploymentOptions deploymentOptions, ClassLoader classLoader,
+      Future<String> resolution) {
+    String identifier = VerticleFactory.removePrefix(id);
+    String descriptorFile = identifier + ".json";
+    try {
+      JsonObject descriptor = readDescriptor(classLoader, descriptorFile);
+      String main = retrieveVerticle(descriptor, descriptorFile);
+
+      // Any options specified in the module config will override anything specified at deployment time
+      // Options and Config specified in separate file with configuration JSON will override those configurations
+      JsonObject depOptions = deploymentOptions.toJson();
+      JsonObject depConfig = depOptions.getJsonObject(CONFIG_KEY, new JsonObject());
+
+      JsonObject verticleOptions = descriptor.getJsonObject(OPTIONS_KEY, new JsonObject());
+      JsonObject verticleConfig = verticleOptions.getJsonObject(CONFIG_KEY, new JsonObject());
+      depOptions.mergeIn(verticleOptions);
+      depOptions.put(CONFIG_KEY, JsonObjectUtil.deepMerge(verticleConfig, depConfig));
+
+      JsonObject serviceDescriptor = new JsonObject().put(OPTIONS_KEY, depOptions);
+
+      deploymentOptions.fromJson(serviceDescriptor.getJsonObject(OPTIONS_KEY));
+      deploymentOptions.setWorker(isWorker());
+      resolution.complete(main);
+    } catch (Exception e) {
+      resolution.fail(e);
+    }
+  }
+
+  @Override
+  public Verticle createVerticle(String verticleName, ClassLoader classLoader) throws Exception {
+    throw new IllegalStateException("Shouldn't be called");
+  }
+
+  protected String retrieveVerticle(JsonObject descriptor, String descriptorFile) {
+    String verticle = descriptor.getString("verticle");
+    if (StringUtils.isEmpty(verticle)) {
+      throw new IllegalArgumentException("There is no verticle name in: " + descriptorFile);
+    }
+    return verticle;
+  }
+
+  abstract protected boolean isWorker();
+
+  private JsonObject readDescriptor(ClassLoader classLoader, String descriptorFile)
+      throws IOException {
+    JsonObject descriptor;
+    try (InputStream is = classLoader.getResourceAsStream(descriptorFile)) {
+      if (is == null) {
+        throw new IllegalArgumentException(
+            "Cannot find module descriptor file " + descriptorFile + " on classpath");
+      }
+      try (Scanner scanner = new Scanner(is, "UTF-8").useDelimiter("\\A")) {
+        String conf = scanner.next();
+        descriptor = new JsonObject(conf);
+      } catch (NoSuchElementException e) {
+        throw new IllegalArgumentException(descriptorFile + " is empty", e);
+      } catch (DecodeException e) {
+        throw new IllegalArgumentException(descriptorFile + " contains invalid json", e);
+      }
+    }
+
+    return descriptor;
+  }
+
+}
