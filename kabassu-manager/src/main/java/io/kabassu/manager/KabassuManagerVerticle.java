a8faba6728293f51843e33ca545d7e4ee8b2a249
Daniel Madejek <dmadejek@gmail.com>
2018-04-29 01:05:52 +0200

    #37 add new store to read from multiple files and started replacing old deployment

--- kabassu-manager/src/main/java/io/kabassu/manager/KabassuManagerVerticle.java
+++ kabassu-manager/src/main/java/io/kabassu/manager/KabassuManagerVerticle.java
@@ -19,11 +19,13 @@ package io.kabassu.manager;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableTransformer;
+import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.reactivex.config.ConfigRetriever;
 import io.vertx.reactivex.core.AbstractVerticle;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
@@ -36,33 +38,73 @@ public class KabassuManagerVerticle extends AbstractVerticle {
   private static final Logger LOGGER = LoggerFactory.getLogger(KabassuManagerVerticle.class);
   private static final String RUNMODE = "runmode";
 
+  private ConfigRetriever configRetriever;
+
   @Override
   public void start(Future<Void> startFuture) throws Exception {
 
     updateOption(config(), RUNMODE, System.getProperty(RUNMODE));
     updateOption(config(), "security", System.getProperty("security"));
 
-    Observable.fromIterable(config().getJsonArray("modules"))
-        .flatMap(module -> deployVerticle(module)
-            .onErrorResumeNext(
-                throwable -> (Observable<Pair<String, String>>) verticleCouldNotBeDeployed(module,
-                    throwable))
-        )
-        .compose(joinDeployments())
-        .subscribe(
-            message -> {
-              LOGGER.info("Kabassu STARTED {}", message);
-              if(config().getJsonObject(CONFIG_OVERRIDE).getString(RUNMODE,"normal").equalsIgnoreCase("demo")){
-                LOGGER.info("Kabassu is running in DEMO Mode");
-              }
-              startFuture.complete();
-            },
-            error -> {
-              LOGGER.error("Verticle could not be deployed", error);
-              startFuture.fail(error);
-            }
-        );
-  }
+    configRetriever = ConfigRetriever.create(vertx, new ConfigRetrieverOptions(config().getJsonObject("modules")));
+
+//    configRetriever.listen(conf -> {
+//      if (!deployedModules.isEmpty()) {
+//        LOGGER.warn("Configuration changed - Re-deploying Knot.x");
+//        Observable.fromIterable(deployedModules)
+//            .flatMap(item -> vertx.rxUndeploy(item.getDeploymentId()).toObservable())
+//            .collect(() -> Lists.newArrayList(),
+//                (collector, result) -> collector.add(result))
+//            .subscribe(
+//                success -> {
+//                  LOGGER.warn("Knot.x STOPPED.");
+//                  deployVerticles(conf.getNewConfiguration(), null);
+//                },
+//                error -> {
+//                  LOGGER.error("Unable to undeploy verticles", error);
+//                  startFuture.fail(error);
+//                }
+//            );
+//      }
+//    });
+
+    configRetriever.listen(conf->{
+      LOGGER.info(conf.getNewConfiguration());
+    });
+
+    configRetriever.getConfig(ar -> {
+      if (ar.succeeded()) {
+        JsonObject configuration = ar.result();
+        LOGGER.info(configuration);
+        //deployVerticles(configuration, startFuture);
+      } else {
+        LOGGER.fatal("Unable to start Knot.x", ar.cause());
+        startFuture.fail(ar.cause());
+      }
+    });
+
+
+//    Observable.fromIterable(config().getJsonArray("modules"))
+//        .flatMap(module -> deployVerticle(module)
+//            .onErrorResumeNext(
+//                throwable -> (Observable<Pair<String, String>>) verticleCouldNotBeDeployed(module,
+//                    throwable))
+//        )
+//        .compose(joinDeployments())
+//        .subscribe(
+//            message -> {
+//              LOGGER.info("Kabassu STARTED {}", message);
+//              if(config().getJsonObject(CONFIG_OVERRIDE).getString(RUNMODE,"normal").equalsIgnoreCase("demo")){
+//                LOGGER.info("Kabassu is running in DEMO Mode");
+//              }
+//              startFuture.complete();
+//            },
+//            error -> {
+//              LOGGER.error("Verticle could not be deployed", error);
+//              startFuture.fail(error);
+//            }
+//        );
+ }
 
   private Observable<Pair<String, String>> verticleCouldNotBeDeployed(Object module,
       Throwable throwable) {
---
+++ kabassu-manager/src/main/java/io/kabassu/manager/configuration/ModulesConfigStore.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2018 Kabassu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.kabassu.manager.configuration;
+
+import io.vertx.config.spi.ConfigProcessor;
+import io.vertx.config.spi.ConfigStore;
+import io.vertx.config.spi.utils.Processors;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.stream.Collectors;
+
+public class ModulesConfigStore implements ConfigStore {
+
+  private Vertx vertx;
+
+  private File path;
+
+  private final ConfigProcessor processor;
+
+  public ModulesConfigStore(Vertx vertx, JsonObject configuration) {
+    this.vertx = vertx;
+    String filePath = configuration.getString("path");
+    if (filePath == null) {
+      throw new IllegalArgumentException("The `path` configuration is required.");
+    }
+    this.path = new File(filePath);
+    if (this.path.isFile()) {
+      throw new IllegalArgumentException("The `path` must not be a file");
+    }
+    this.processor = Processors.get("json");
+  }
+
+
+  @Override
+  public void get(Handler<AsyncResult<Buffer>> completionHandler) {
+    vertx.<List<File>>executeBlocking(
+        fut -> {
+          try {
+            fut.complete(Arrays.asList(path.listFiles()).stream().filter(File::isFile)
+                .collect(Collectors.toList()));
+          } catch (Exception e) {
+            fut.fail(e);
+          }
+        },
+        ar -> {
+          if (ar.failed()) {
+            completionHandler.handle(Future.failedFuture(ar.cause()));
+          } else {
+            buildConfiguration(ar.result(), json -> {
+              if (json.failed()) {
+                completionHandler.handle(Future.failedFuture(ar.cause()));
+              } else {
+                completionHandler
+                    .handle(Future.succeededFuture(Buffer.buffer(new JsonObject().put("modules",json.result()).encode())));
+              }
+            });
+          }
+        }
+    );
+  }
+
+  public void buildConfiguration(List<File> files, Handler<AsyncResult<JsonArray>> handler) {
+    List<Future> futures = new ArrayList<>();
+    files.stream()
+        .forEach(file -> {
+          Future<JsonObject> future = Future.future();
+          futures.add(future);
+          try {
+            vertx.fileSystem().readFile(file.getAbsolutePath(),
+                buffer -> {
+                  if (buffer.failed()) {
+                    future.fail(buffer.cause());
+                  } else {
+                    processor.process(vertx, null, buffer.result(), future.completer());
+                  }
+                });
+          } catch (RejectedExecutionException e) {
+            future.fail(e);
+          }
+        });
+
+    CompositeFuture.all(futures).setHandler(ar -> {
+      if (ar.failed()) {
+        handler.handle(Future.failedFuture(ar.cause()));
+      } else {
+        JsonArray result = new JsonArray();
+        futures.stream()
+            .map(future -> (JsonObject) future.result())
+            .forEach(result::add);
+        handler.handle(Future.succeededFuture(result));
+      }
+    });
+  }
+}
