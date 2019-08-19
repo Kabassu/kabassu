15f19ef0b094d2e8de1d894426328e83a6dfd68a
Daniel Madejek <daniel.madejek@cognifide.com>
2019-08-19 15:12:14 +0200

    counting items

--- kabassu-mongo/src/main/java/io/kabassu/mongo/handlers/MongoGetAllHandler.java
+++ kabassu-mongo/src/main/java/io/kabassu/mongo/handlers/MongoGetAllHandler.java
@@ -5,6 +5,8 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.mongo.FindOptions;
+import io.vertx.reactivex.core.CompositeFuture;
+import io.vertx.reactivex.core.Promise;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.eventbus.Message;
 
@@ -20,16 +22,63 @@ public class MongoGetAllHandler extends AbstractMongoHandler<JsonObject> {
   @Override
   public void handle(Message<JsonObject> event) {
     JsonObject request = event.body();
+    Promise<JsonObject> getDataPromise = getData(request);
+    Promise<Long> countItemsPromise = countItems(request.getString("collection"));
+    CompositeFuture
+      .all(getDataPromise.future(), countItemsPromise.future())
+      .setHandler(
+        completedFutures -> {
+          if (completedFutures.succeeded()) {
+            JsonObject result = getDataPromise.future().result();
+            result.put("allItems", countItemsPromise.future().result());
+            event.reply(result);
+          } else {
+            event.reply("ERROR");
+          }
+        });
+  }
+
+  private Promise<JsonObject> getData(JsonObject request) {
+    Promise<JsonObject> promise = Promise.promise();
     FindOptions findOptions = new FindOptions();
     findOptions.setLimit(request.getInteger("pageSize"));
-    findOptions.setSkip(request.getInteger("pageSize")*request.getInteger("page"));
-    client.findWithOptions(request.getString("collection"), new JsonObject(), findOptions, res->{
+    findOptions.setSkip(request.getInteger("pageSize") * request.getInteger("page"));
+    client.findWithOptions(request.getString("collection"), new JsonObject(), findOptions, res -> {
       if (res.succeeded()) {
-        event.reply(new JsonObject().put("results",res.result()));
+        promise.complete(new JsonObject().put("results", res.result()));
       } else {
-        event.reply(new JsonObject().put("error", res.cause().getMessage()));
+        promise.complete(new JsonObject().put("error", res.cause().getMessage()));
         LOGGER.error("Problem during retrieving data", res.cause());
       }
     });
+    try {
+      return promise;
+    } catch (Exception e) {
+      LOGGER.error("Error during getting data from collection {}",
+        request.getString("collection"), e);
+      promise.complete(new JsonObject().put("error", e.getMessage()));
+      LOGGER.error("Problem during retrieving data", e);
+      return promise;
+    }
+  }
+
+  private Promise<Long> countItems(String collection) {
+    Promise<Long> promise = Promise.promise();
+    client.count(collection, new JsonObject(), res -> {
+      if (res.succeeded()) {
+        long objects = res.result();
+        promise.complete(objects);
+      } else {
+        promise.complete(0L);
+      }
+    });
+    try {
+      return promise;
+    } catch (Exception e) {
+      LOGGER.error("Error during counting objects in collection {}",
+        collection, e);
+      promise.complete(0l);
+      return promise;
+    }
   }
 }
