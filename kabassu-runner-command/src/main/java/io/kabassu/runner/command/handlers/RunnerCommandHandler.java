16002ae1a18faf9df9def5bb2db7c4fbb9a873f8
Daniel Madejek <daniel.madejek@cognifide.com>
2019-09-30 15:11:47 +0200

    Finished Command Handler

--- kabassu-runner-command/src/main/java/io/kabassu/runner/command/handlers/RunnerCommandHandler.java
+++ kabassu-runner-command/src/main/java/io/kabassu/runner/command/handlers/RunnerCommandHandler.java
@@ -19,6 +19,7 @@ package io.kabassu.runner.command.handlers;
 
 import io.kabassu.commons.checks.FilesDownloadChecker;
 import io.kabassu.commons.configuration.ConfigurationRetriever;
+import io.kabassu.commons.constants.CommandLines;
 import io.kabassu.commons.constants.JsonFields;
 import io.kabassu.runner.command.configuration.KabassuRunnerCommandConfiguration;
 import io.vertx.core.Handler;
@@ -27,13 +28,12 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.core.eventbus.Message;
-import java.io.ByteArrayInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
-import org.gradle.tooling.BuildException;
-import org.gradle.tooling.BuildLauncher;
-import org.gradle.tooling.GradleConnector;
-import org.gradle.tooling.ProjectConnection;
+import org.apache.commons.lang3.SystemUtils;
 
 public class RunnerCommandHandler implements Handler<Message<JsonObject>> {
 
@@ -52,66 +52,69 @@ public class RunnerCommandHandler implements Handler<Message<JsonObject>> {
   public void handle(Message<JsonObject> event) {
 
     if (FilesDownloadChecker
-        .checkIfFilesRetrieveIsRequired(
-            event.body().getJsonObject(JsonFields.DEFINITION).getString("locationType"))) {
+      .checkIfFilesRetrieveIsRequired(
+        event.body().getJsonObject(JsonFields.DEFINITION).getString("locationType"))) {
       vertx.eventBus().rxRequest("kabassu.filesretriever",
-          event.body()).toObservable()
-          .doOnNext(
-              eventResponse ->
-                  runTest((JsonObject) eventResponse.body())
-          ).subscribe();
+        event.body()).toObservable()
+        .doOnNext(
+          eventResponse ->
+            runTest((JsonObject) eventResponse.body())
+        ).subscribe();
     } else {
       runTest(event.body());
     }
   }
 
   private void runTest(JsonObject fullRequest) {
-    String testResult = "Failure";
+    String testResult = "Success";
     JsonObject testDefinition = fullRequest.getJsonObject(JsonFields.DEFINITION);
-    try (ProjectConnection connection = GradleConnector.newConnector()
-        .forProjectDirectory(
-            new File(ConfigurationRetriever.getParameter(testDefinition, "location"))).connect()) {
-      BuildLauncher buildLauncher = connection.newBuild();
-      if (ConfigurationRetriever.containsParameter(testDefinition, "runnerOptions")) {
-        String[] runnerOptions = ConfigurationRetriever
-            .getParameter(testDefinition, "runnerOptions").split(" ");
-        buildLauncher.forTasks(runnerOptions);
-      } else {
-        buildLauncher.forTasks(new String[1]);
-      }
-      if (ConfigurationRetriever
-          .containsParameter(fullRequest.getJsonObject(JsonFields.TEST_REQUEST)
-              , "jvm")) {
-        buildLauncher.setJavaHome(new File(configuration.getJvmsMap()
-            .get(ConfigurationRetriever
-                .getParameter(fullRequest.getJsonObject(JsonFields.TEST_REQUEST), "jvm"))));
-      }
-      buildLauncher.setStandardInput(new ByteArrayInputStream("consume this!".getBytes()));
-      buildLauncher.run();
-      testResult = "Success";
-    } catch (BuildException e) {
-      LOGGER.error(e);
-    } finally {
-      final String result = testResult;
-      JsonObject updateHistory = updateHistory(fullRequest.getJsonObject(JsonFields.TEST_REQUEST),
-          testResult);
-      vertx.eventBus().rxRequest("kabassu.database.mongo.replacedocument", updateHistory)
-          .toObservable()
-          .doOnNext(
-              eventResponse ->
-                  vertx.eventBus().send("kabassu.results.dispatcher",
-                      fullRequest.put("result", result)
-                          .put(JsonFields.TEST_REQUEST, updateHistory.getJsonObject("new")))
+    if (ConfigurationRetriever.containsParameter(testDefinition, "runnerOptions")) {
+      String runnerOptions = ConfigurationRetriever
+        .getParameter(testDefinition, "runnerOptions");
 
-          ).subscribe();
+      ProcessBuilder processBuilder = new ProcessBuilder();
+      processBuilder
+        .directory(new File(ConfigurationRetriever.getParameter(testDefinition, "location")));
+      if (SystemUtils.IS_OS_WINDOWS) {
+        processBuilder.command(CommandLines.CMD, "/c", runnerOptions);
+      } else {
+        processBuilder.command(CommandLines.BASH, "-c", runnerOptions);
+      }
+      try {
+        Process process = processBuilder.start();
+        BufferedReader in = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        String line;
+        if((line = in.readLine()) != null) {
+          testResult = "Failure";
+        }
+
+        process.waitFor();
+      } catch (IOException | InterruptedException ex) {
+        LOGGER.error(ex);
+        testResult = "Failure";
+      }
     }
+
+    final String result = testResult;
+    JsonObject updateHistory = updateHistory(fullRequest.getJsonObject(JsonFields.TEST_REQUEST),
+      testResult);
+    vertx.eventBus().rxRequest("kabassu.database.mongo.replacedocument", updateHistory)
+      .toObservable()
+      .doOnNext(
+        eventResponse ->
+          vertx.eventBus().send("kabassu.results.dispatcher",
+            fullRequest.put("result", result)
+              .put(JsonFields.TEST_REQUEST, updateHistory.getJsonObject("new")))
+
+      ).subscribe();
+
   }
 
   private JsonObject updateHistory(JsonObject testRequest, String testResult) {
     testRequest.getJsonArray("history").add(new JsonObject().put("date", new Date().getTime())
-        .put("event", "Test finished with: " + testResult));
+      .put("event", "Test finished with: " + testResult));
     return new JsonObject().put("new", testRequest)
-        .put(JsonFields.COLLECTION, "kabassu-requests").put("id", testRequest.getString("_id"));
+      .put(JsonFields.COLLECTION, "kabassu-requests").put("id", testRequest.getString("_id"));
   }
 
 }
